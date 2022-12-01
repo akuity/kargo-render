9759a17ced22c82882d2264293d61659db491e62
Kent <kent.rancourt@gmail.com>
2022-11-30 21:45:27 -0500

    fix image substitution logic
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- rendering.go
+++ rendering.go
@@ -1,16 +1,20 @@
 package bookkeeper
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/ghodss/yaml"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/akuityio/bookkeeper/internal/config"
 	"github.com/akuityio/bookkeeper/internal/git"
 	"github.com/akuityio/bookkeeper/internal/helm"
 	"github.com/akuityio/bookkeeper/internal/kustomize"
+	"github.com/akuityio/bookkeeper/internal/metadata"
 	"github.com/akuityio/bookkeeper/internal/ytt"
-	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 )
 
 var lastMileKustomizationBytes = []byte(
@@ -71,12 +75,14 @@ func (s *service) preRender(
 
 func (s *service) renderLastMile(
 	repo git.Repo,
+	oldTargetBranchMetadata metadata.TargetBranchMetadata,
+	intermediateMetadata *metadata.TargetBranchMetadata,
 	req RenderRequest,
 	preRenderedBytes []byte,
-) ([]byte, error) {
+) ([]string, []byte, error) {
 	tempDir, err := os.MkdirTemp("", "")
 	if err != nil {
-		return nil, errors.Wrapf(
+		return nil, nil, errors.Wrapf(
 			err,
 			"error creating temporary directory %q for last mile rendering",
 			tempDir,
@@ -88,7 +94,7 @@ func (s *service) renderLastMile(
 	preRenderedPath := filepath.Join(tempDir, "all.yaml")
 	// nolint: gosec
 	if err = os.WriteFile(preRenderedPath, preRenderedBytes, 0644); err != nil {
-		return nil, errors.Wrapf(
+		return nil, nil, errors.Wrapf(
 			err,
 			"error writing pre-rendered configuration to %q",
 			preRenderedPath,
@@ -102,17 +108,40 @@ func (s *service) renderLastMile(
 		lastMileKustomizationBytes,
 		0644,
 	); err != nil {
-		return nil, errors.Wrapf(
+		return nil, nil, errors.Wrapf(
 			err,
 			"error writing to %q",
 			kustomizationFile,
 		)
 	}
 
-	// Apply new images if any were specified
+	// Apply new images from old target branch metadata if any were specified
+	for _, image := range oldTargetBranchMetadata.ImageSubstitutions {
+		if err = kustomize.SetImage(tempDir, image); err != nil {
+			return nil, nil, errors.Wrapf(
+				err,
+				"error applying new image %q",
+				image,
+			)
+		}
+	}
+	// Apply new images from intermediate metadata if any were specified
+	if intermediateMetadata != nil {
+		for _, image := range intermediateMetadata.ImageSubstitutions {
+			if err = kustomize.SetImage(tempDir, image); err != nil {
+				return nil, nil, errors.Wrapf(
+					err,
+					"error applying new image %q",
+					image,
+				)
+			}
+		}
+	}
+	// Apply new images from the request if any were specified. These will take
+	// precedence over anything we already set.
 	for _, image := range req.Images {
 		if err = kustomize.SetImage(tempDir, image); err != nil {
-			return nil, errors.Wrapf(
+			return nil, nil, errors.Wrapf(
 				err,
 				"error applying new image %q",
 				image,
@@ -120,8 +149,29 @@ func (s *service) renderLastMile(
 		}
 	}
 
+	// Read images back from kustomization.yaml. This is a convenient way to
+	// collapse images from intermediate metadata and request images into a single
+	// list of images, with precedence given to the latter.
+	kustomization := struct {
+		Images []struct {
+			Name   string `json:"name,omitempty"`
+			NewTag string `json:"newTag,omitempty"`
+		} `json:"images,omitempty"`
+	}{}
+	kustomizationBytes, err := os.ReadFile(kustomizationFile)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "error reading kustomization.yaml")
+	}
+	if err = yaml.Unmarshal(kustomizationBytes, &kustomization); err != nil {
+		return nil, nil, errors.Wrap(err, "error unmarshaling kustomization.yaml")
+	}
+	images := make([]string, len(kustomization.Images))
+	for i, image := range kustomization.Images {
+		images[i] = fmt.Sprintf("%s:%s", image.Name, image.NewTag)
+	}
+
 	fullyRenderedBytes, err := kustomize.LastMileRender(tempDir)
-	return fullyRenderedBytes, errors.Wrapf(
+	return images, fullyRenderedBytes, errors.Wrapf(
 		err,
 		"error rendering configuration from %q",
 		tempDir,
