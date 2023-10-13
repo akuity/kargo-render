0e5cfcd485abf2a16190348d56d6ba540358bd14
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2023-10-13 15:41:30 -0700

    refactor: reuse Argo CD types to manage manifest generation
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- config.go
+++ config.go
@@ -12,10 +12,8 @@ import (
 	"github.com/pkg/errors"
 	"github.com/xeipuuv/gojsonschema"
 
+	"github.com/akuity/kargo-render/internal/argocd"
 	"github.com/akuity/kargo-render/internal/file"
-	"github.com/akuity/kargo-render/internal/helm"
-	"github.com/akuity/kargo-render/internal/kustomize"
-	"github.com/akuity/kargo-render/internal/ytt"
 
 	_ "embed"
 )
@@ -48,7 +46,7 @@ func (r *repoConfig) GetBranchConfig(name string) (branchConfig, error) {
 			}
 			submatches := regex.FindStringSubmatch(name)
 			if len(submatches) > 0 {
-				return cfg.expand(submatches), nil
+				return cfg.expand(submatches)
 			}
 		}
 	}
@@ -80,23 +78,31 @@ type branchConfig struct {
 	PreservedPaths []string `json:"preservedPaths,omitempty"`
 }
 
-func (b branchConfig) expand(values []string) branchConfig {
+func (b branchConfig) expand(values []string) (branchConfig, error) {
 	cfg := b
 	cfg.AppConfigs = map[string]appConfig{}
 	for appName, appConfig := range b.AppConfigs {
-		cfg.AppConfigs[appName] = appConfig.expand(values)
+		var err error
+		if cfg.AppConfigs[appName], err = appConfig.expand(values); err != nil {
+			return cfg, errors.Wrapf(
+				err,
+				"error expanding app config for app %q",
+				appName,
+			)
+		}
 	}
+
 	for i, path := range b.PreservedPaths {
 		b.PreservedPaths[i] = file.ExpandPath(path, values)
 	}
-	return cfg
+	return cfg, nil
 }
 
 // appConfig encapsulates application-specific Kargo Render configuration.
 type appConfig struct {
 	// ConfigManagement encapsulates configuration management options to be
 	// used with this branch and app.
-	ConfigManagement configManagementConfig `json:"configManagement,omitempty"`
+	ConfigManagement argocd.ConfigManagementConfig `json:"configManagement"`
 	// OutputPath specifies a path relative to the root of the repository where
 	// rendered manifests for this app will be stored in this branch.
 	OutputPath string `json:"outputPath,omitempty"`
@@ -105,40 +111,14 @@ type appConfig struct {
 	CombineManifests bool `json:"combineManifests,omitempty"`
 }
 
-func (a appConfig) expand(values []string) appConfig {
+func (a appConfig) expand(values []string) (appConfig, error) {
 	cfg := a
-	cfg.ConfigManagement = a.ConfigManagement.expand(values)
+	var err error
+	if cfg.ConfigManagement, err = a.ConfigManagement.Expand(values); err != nil {
+		return cfg, errors.Wrap(err, "error expanding config management config")
+	}
 	cfg.OutputPath = file.ExpandPath(a.OutputPath, values)
-	return cfg
-}
-
-// configManagementConfig is a wrapper around more specific configuration for
-// one of three supported configuration management tools: helm, kustomize, or
-// ytt. Only one of its fields may be non-nil.
-type configManagementConfig struct { // nolint: revive
-	// Helm encapsulates optional Helm configuration options.
-	Helm *helm.Config `json:"helm,omitempty"`
-	// Kustomize encapsulates optional Kustomize configuration options.
-	Kustomize *kustomize.Config `json:"kustomize,omitempty"`
-	// Ytt encapsulates optional ytt configuration options.
-	Ytt *ytt.Config `json:"ytt,omitempty"`
-}
-
-func (c configManagementConfig) expand(values []string) configManagementConfig {
-	cfg := c
-	if c.Helm != nil {
-		helmCfg := c.Helm.Expand(values)
-		cfg.Helm = &helmCfg
-	}
-	if c.Kustomize != nil {
-		kustomizeCfg := c.Kustomize.Expand(values)
-		cfg.Kustomize = &kustomizeCfg
-	}
-	if c.Ytt != nil {
-		yttCfg := c.Ytt.Expand(values)
-		cfg.Ytt = &yttCfg
-	}
-	return cfg
+	return cfg, nil
 }
 
 // pullRequestConfig encapsulates details related to PR management for a branch.
--- docs/docs/50-roadmap.md
+++ docs/docs/50-roadmap.md
@@ -9,4 +9,8 @@ Kargo Render is highly experimental at this time and breaking changes should be
 anticipated between pre-GA minor releases.
 :::
 
-Placeholder
+## Argo CD Config Management Plugins
+
+An ability to configure and use Argo CD
+[config management plugins](https://argo-cd.readthedocs.io/en/stable/operator-manual/config-management-plugins/)
+for manifest generation.
