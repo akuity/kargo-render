173cd781f2da9eae24e6e40d849404431b67ebfc
Kent <kent.rancourt@gmail.com>
2023-10-24 13:23:53 -0400

    send all log output to stderr
    Signed-off-by: Kent <kent.rancourt@gmail.com>

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -8,18 +8,15 @@ import (
 )
 
 func Run() {
-	// These two lines are required to suppress log output from the Argo CD repo
-	// server, which Kargo Render uses as a library.
-	//
-	// Without suppressing this, requests for machine-readable output (e.g. JSON)
-	// will be polluted with log output and attempts to parse it (e.g. by Kargo)
-	// will fail.
-	//
-	// This does NOT interfere with using the Kargo Render CLI's own --debug flag,
-	// however, choosing that will, once again, result in some amount of
-	// unparsable output.
+	// These two lines are required to suppress undesired log output from the Argo
+	// CD repo server, which Kargo Render uses as a library. This does NOT
+	// interfere with using the Kargo Render CLI's own --debug flag.
 	os.Setenv("ARGOCD_LOG_LEVEL", "PANIC")
 	log.SetLevel(log.PanicLevel)
+	// This line makes all log output go to stderr, leaving stdout for actual
+	// program output only. This is important for cases where machine readable
+	// output (e.g. JSON) is requested.
+	log.SetOutput(os.Stderr)
 
 	cmd, err := newRootCommand()
 	if err != nil {
